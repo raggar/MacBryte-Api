4e836cbc12927d1dfe89e69107e927aeb29f572d
Rahul Aggarwal <rahulaggarwal1016@gmail.com>
2021-07-29 00:38:02 -0400

    fixed api

--- src/controllers.ts
+++ src/controllers.ts
@@ -108,13 +108,13 @@ const updateUsersHandler: RequestHandler = async (req, res, next) => {
             return null;
           }
         });
-        await Promise.all(updateUserPromises);
-        res.status(200).json({
-          error: false,
-          requestMessage: "Users successfully updated",
-        });
       }
     );
+    await Promise.all(updateUserPromises);
+    res.status(200).json({
+      error: false,
+      requestMessage: "Users successfully updated",
+    });
   } catch (err) {
     next(err);
   }
